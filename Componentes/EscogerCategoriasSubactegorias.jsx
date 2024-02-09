dd3a962908e6a2bf054765e34b1832162e367113
Daniel Galindo <galijine@gmail.com>
2024-02-09 09:47:14 -0500

    Añadir estilos y botones a la pantalla de EscogerCategoriasSubactegorias (funcional)

--- Componentes/EscogerCategoriasSubactegorias.jsx
+++ Componentes/EscogerCategoriasSubactegorias.jsx
@@ -12,13 +12,49 @@ import Logo from "../assets/imgs/jucar.jpg";
 
 const EscogerCategoriasSubactegorias = () => {
   return (
-    <View style={styles.conatiner}>
-      <View style={styles.navbar}>
-        <Image source={Logo} style={styles.logo} />
+    <>
+      <View style={styles.conatiner}>
+        <View style={styles.navbar}>
+          <Image source={Logo} style={styles.logo} />
 
-        <Text style={styles.title}>AUTOPARTES JUCAR SAS</Text>
+          <Text style={styles.title}>AUTOPARTES JUCAR SAS</Text>
+        </View>
       </View>
-    </View>
+      <View>
+        <Text style={styles.title2}> {"\n"} CATEGORIAS</Text>
+
+        <Pressable
+          style={styles.botones}
+          onPress={() => navigation.navigate("AllAutoparts")}
+        >
+          <Text style={styles.botonesText}>Lista de Categorias</Text>
+        </Pressable>
+
+        <Pressable
+          style={styles.botones}
+          onPress={() => navigation.navigate("AutoparteID")}
+        >
+          <Text style={styles.botonesText}>Crear Categoria</Text>
+        </Pressable>
+      </View>
+      <View>
+        <Text style={styles.title3}> {"\n"} SUBCATEGORIAS</Text>
+
+        <Pressable
+          style={styles.botones}
+          onPress={() => navigation.navigate("AllAutoparts")}
+        >
+          <Text style={styles.botonesText}>Todas las SubCategorias</Text>
+        </Pressable>
+
+        <Pressable
+          style={styles.botones}
+          onPress={() => navigation.navigate("AutoparteID")}
+        >
+          <Text style={styles.botonesText}>Subcategoria por Categoria</Text>
+        </Pressable>
+      </View>
+    </>
   );
 };
 const styles = StyleSheet.create({
@@ -56,5 +92,54 @@ const styles = StyleSheet.create({
     height: 68,
   },
   //logo <-
+
+  title2: {
+    fontSize: 18,
+    color: "red ",
+    fontWeight: "bold",
+    marginLeft: 128,
+    marginRight: -21,
+    marginBottom: -19,
+    width: 269.906,
+    height: 68,
+  },
+
+  //botones
+  botonesContainer: {
+    height: "70%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "10%",
+  },
+  botones: {
+    padding: 5,
+    backgroundColor: "#f80759",
+    borderColor: "#000000",
+    borderWidth: 3,
+    borderRadius: 10,
+    fontSize: 25,
+    width: "80%",
+    margin: 5,
+    boxshadowColor: "#000000",
+    boxshadowOpacity: 0,
+    fontFamily: "sans-",
+  },
+  botonesText: {
+    textAlign: "center",
+    fontSize: 25,
+    color: "#fff",
+    textTransform: "uppercase",
+  },
+  title3: {
+    fontSize: 18,
+    color: "red ",
+    fontWeight: "bold",
+    marginLeft: 128,
+    marginRight: -21,
+    marginBottom: -19,
+    width: 269.906,
+    height: 68,
+  },
 });
 export default EscogerCategoriasSubactegorias;
