aa31ee069d3fc6f77da6b7bc677192b3e035b147
Daniel Galindo <galijine@gmail.com>
2024-02-26 19:46:44 -0500

    poner barra superior para salir y dar marcha atras

--- App.js
+++ App.js
@@ -1,100 +1,283 @@
-// import Usuers from "./Componentes/Users"; // Mal importado 'Usuers';
-// import Users from "./Componentes/Users";
-// import AllAutopartes from "./Componentes/AllAutoparts"; // Tenía mal el nombre del componente 'AllAutopartes' y es 'AllAutoparts';
+import React, { useState } from "react";
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  Pressable,
+} from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 import Login from "./Componentes/Login";
 import Menu from "./Componentes/Menu";
 import Proveedores from "./Componentes/Proveedores";
 import Productos from "./Componentes/Productos";
 import EscogerAutoparte from "./Componentes/EscogerAutoparte";
-import AllAutoparts from "./Componentes/AllAutoparts";
-import { NavigationContainer } from "@react-navigation/native";
+//import AllAutoparts from "./Componentes/AllAutoparts";
 import EscogerCategoriasSubactegorias from "./Componentes/EscogerCategoriasSubactegorias";
 import AllCategories from "./Componentes/AllCategories";
 import Subcategorias from "./Componentes/Subcategorias";
+import MenuSubcategories from "./Componentes/Menus/MenuSubcategories";
+import salir from "./assets/imgs/salir.jpg";
+import Registro from "./Componentes/Registro";
+
 const Stack = createNativeStackNavigator();
 
-//
-//  const api = axios.create({
-//    baseUrl: "url api",
-//    timeout : 10000
-//  })
-
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado de autenticación
+
+  const handleLogin = () => {
+    // Simulacion de autenticacion exitosa
+    setIsAuthenticated(true);
+    navigation.navigate("Menu");
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    navigation.navigate("Login");
+  };
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator>
         <Stack.Screen
           name="Login"
           component={Login}
           options={{ headerShown: false, title: "Login" }}
+          listeners={({ navigation }) => ({
+            tabPress: (event) => {
+              handleLogin();
+            },
+          })}
         />
-
+        <Stack.Screen name="Registro" component={Registro} />
         <Stack.Screen
           name="Menu"
           component={Menu}
-          options={{ headerShown: false, title: "Menu" }}
+          options={{
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            title: "Inicio",
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
+          }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
         />
-
         <Stack.Screen
           name="Productos"
           component={Productos}
-          options={{ headerShown: false, title: "Productos" }}
+          options={{
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Menu")}>
+                <Text> Inicio </Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
+          }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
         />
-
-        {/* Ventas no está importado, porque lo borré al ser un componente vacío */}
-
-        {/* <Stack.Screen
-          name="Ventas"
-          component={Ventas}
-          options={{ headerShown: false, title: "Ventas" }}
-        /> */}
-
         <Stack.Screen
           name="Proveedores"
           component={Proveedores}
-          options={{ headerShown: false, title: "Proveedores" }}
+          options={{
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Menu")}>
+                <Text> Inicio </Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
+          }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
         />
-
         <Stack.Screen
           name="EscogerAutoparte"
           component={EscogerAutoparte}
-          options={{ headerShown: false, title: "EscogerAutoparte" }}
+          options={{
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Menu")}>
+                <Text> Inicio </Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
+          }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
         />
-
         {/* <Stack.Screen
           name="AllAutoparts"
           component={AllAutoparts}
-          options={{ headerShown: false, title: "AllAutoparts" }}
+          options={{
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Menu")}>
+                <Text> Inicio </Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
+          }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
         /> */}
-
         <Stack.Screen
           name="EscogerCategoriasSubactegorias"
+          title="Todas las Categorias"
           component={EscogerCategoriasSubactegorias}
           options={{
-            headerShown: false,
-            title: "EscogerCategoriasSubactegorias",
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Menu")}>
+                <Text> Inicio </Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
           }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
         />
         <Stack.Screen
           name="AllCategories"
+          title="Todas las Categorias"
           component={AllCategories}
           options={{
-            headerShown: false,
-            title: "AllCategories",
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Menu")}>
+                <Text> Inicio </Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
           }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
         />
         <Stack.Screen
           name="Subcategorias"
           component={Subcategorias}
           options={{
-            headerShown: false,
-            title: "Subcategorias",
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Menu")}>
+                <Text> Inicio </Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
           }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
+        />
+        <Stack.Screen
+          name="MenuSubcategories"
+          component={MenuSubcategories}
+          options={{
+            tabBarOptions: {
+              position: "bottom",
+              style: styles.tabBar, // Aplica el estilo de la barra inferior
+            },
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Menu")}>
+                <Text> Inicio </Text>
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => handleLogout()}>
+                <Image source={salir} style={styles.logo}></Image>
+              </TouchableOpacity>
+            ),
+          }}
+          listeners={({ navigation }) => ({
+            beforeRemove: () => !isAuthenticated,
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+const styles = StyleSheet.create({
+  tabBar: {
+    position: "bottom",
+    height: 50,
+    border: {
+      width: 1,
+      color: "#ccc",
+    },
+    backgroundColor: "#000",
+    color: "#fff",
+  },
+  logo: {
+    width: 37,
+    height: 57,
+    resizeMode: "contain",
+    marginLeft: 50,
+  },
+});
 
 export default App;
