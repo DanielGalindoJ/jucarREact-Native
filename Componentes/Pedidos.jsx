cd60c750c0bf52362939fb7769d9f361ef324149
Daniel Galindo <galijine@gmail.com>
2024-03-17 14:06:34 -0500

    arreglo de pedido, todavia no me sale el get

--- Componentes/Pedidos.jsx
+++ Componentes/Pedidos.jsx
@@ -1,286 +1,216 @@
 import React, { useState, useEffect } from "react";
 import {
   View,
-  Button,
-  Modal,
-  TextInput,
   Text,
-  FlatList,
-  Picker,
+  TextInput,
+  Button,
+  ScrollView,
   StyleSheet,
+  Modal,
+  Picker,
 } from "react-native";
-import axios from "axios";
 
-const Orders = ({ customerId }) => {
+const Pedidos = ({ customerId }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const currentDate = new Date();
   const formattedDate = currentDate.toISOString().split("T")[0];
-  const [orders, setOrders] = useState([]);
-  const [autoparts, setAutoparts] = useState([]);
-  const [showModal, setShowModal] = useState(false);
   const [newOrder, setNewOrder] = useState({
     OrderDate: formattedDate,
     PaymentStatus: "",
     ShippingAddress: "",
     ShippingStatus: "",
     Observation: "",
-    OrderDetails: [],
+    OrderDetails: [
+      {
+        AutopartId: "",
+        Quantity: 0,
+      },
+    ],
+    Contributions: [
+      {
+        PaymentMethodId: "",
+        AmountPaid: 0,
+        ContributionDate: formattedDate,
+      },
+    ],
   });
-  const [error, setError] = useState(null);
+  const [autoparts, setAutoparts] = useState([]);
 
   useEffect(() => {
     const fetchAutoparts = async () => {
       try {
-        const response = await axios.get(
-          "https://localhost:7028/api/autoparts"
-        );
-        setAutoparts(response.data);
+        const response = await fetch("https://localhost:7028/api/autoparts");
+        const data = await response.json();
+        setAutoparts(data);
       } catch (error) {
-        setError(error);
         console.error("Error fetching autoparts:", error);
       }
     };
-
-    const fetchOrders = async () => {
-      try {
-        if (!customerId) return;
-        const response = await axios.get(
-          `https://localhost:7028/api/customers/${customerId}/orders`
-        );
-        const mappedOrders = response.data.map((order) => ({
-          orderId: order.orderID,
-          orderDate: order.orderDate,
-          paymentStatus: order.paymentStatus,
-          shippingAddress: order.shippingAddress,
-          shippingStatus: order.shippingStatus,
-          observation: order.Observation,
-          OrderDetails: order.orderDetails,
-        }));
-        setOrders(mappedOrders);
-        console.log(mappedOrders);
-      } catch (error) {
-        setError(error);
-        console.error("Error fetching orders:", error);
-      }
-    };
-
     fetchAutoparts();
-    fetchOrders();
-  }, [customerId]);
+  }, []);
 
   const handleCreateOrder = async () => {
     try {
-      const response = await axios.post(
-        `https://localhost:7028/api/customers/${customerId}/orders`,
-        newOrder
-      );
-      if (response.status === 201) {
-        const createdOrder = {
-          OrderId: response.data.orderID,
-          OrderDate: response.data.orderDate,
-          PaymentStatus: response.data.paymentStatus,
-          ShippingAddress: response.data.shippingAddress,
-          ShippingStatus: response.data.shippingStatus,
-          Observation: response.data.observation,
-          OrderDetails: response.data.orderDetails,
-        };
-        setOrders([createdOrder, ...orders]);
-        console.log(createdOrder);
-        handleCloseModal();
-      } else {
-        console.error("Unexpected status code:", response.status);
-      }
+      console.log("Creating order with the next data:", newOrder);
+      // Aquí puedes realizar la solicitud POST para crear un pedido
+      setIsModalVisible(false);
+      alert("¡Éxito! El pedido ha sido creado exitosamente.");
     } catch (error) {
-      setError(error);
-      console.error("Error creating order:", error);
-      if (error.response) {
-        console.error("Server error message:", error.response.data);
-      }
+      console.error("Error creating an order:", error);
+      alert("Error: Hubo un problema al crear el pedido.");
     }
   };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
+  const handleDetailChange = (index, field, value) => {
+    const updatedDetails = [...newOrder.OrderDetails];
+    updatedDetails[index][field] = value;
+    setNewOrder({ ...newOrder, OrderDetails: updatedDetails });
+  };
+
+  const addOrderDetail = () => {
     setNewOrder({
-      OrderDate: formattedDate,
-      PaymentStatus: "",
-      ShippingAddress: "",
-      ShippingStatus: "",
-      Observation: "",
-      OrderDetails: [],
+      ...newOrder,
+      OrderDetails: [...newOrder.OrderDetails, { AutopartId: "", Quantity: 0 }],
     });
   };
 
-  const renderItem = ({ item }) => (
-    <View style={styles.orderItem} key={item.orderId}>
-      <Text>Fecha del pedido: {item.orderDate}</Text>
-      <Text>Estado de Pago: {item.paymentStatus}</Text>
-      <Text>Dirección de Envío: {item.shippingAddress}</Text>
-      <Text>Estado de Envío: {item.shippingStatus}</Text>
-      <Text>Observación: {item.observation}</Text>
-      <Text>cevwec</Text>
-    </View>
-  );
+  const removeOrderDetail = (index) => {
+    const updatedOrderDetails = [...newOrder.OrderDetails];
+    updatedOrderDetails.splice(index, 1);
+    setNewOrder({ ...newOrder, OrderDetails: updatedOrderDetails });
+  };
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={orders}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.OrderId.toString()}
-      />
+    <ScrollView>
+      <View style={{ padding: 20 }}>
+        {/* Formulario de creación de pedido */}
+        <Button title="Nuevo Pedido" onPress={() => setIsModalVisible(true)} />
 
-      <Button title="Crear Pedido" onPress={() => setShowModal(true)} />
-
-      <Modal visible={showModal} animationType="slide">
-        <View style={styles.modalContainer}>
-          <Text style={styles.modalTitle}>Crear Nuevo Pedido</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Estado de Pago"
-            value={newOrder.PaymentStatus}
-            onChangeText={(text) =>
-              setNewOrder({ ...newOrder, PaymentStatus: text })
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Dirección de Envío"
-            value={newOrder.ShippingAddress}
-            onChangeText={(text) =>
-              setNewOrder({ ...newOrder, ShippingAddress: text })
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Estado de Envío"
-            value={newOrder.ShippingStatus}
-            onChangeText={(text) =>
-              setNewOrder({ ...newOrder, ShippingStatus: text })
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Observación"
-            value={newOrder.Observation}
-            onChangeText={(text) =>
-              setNewOrder({ ...newOrder, Observation: text })
-            }
-          />
-          <View style={styles.orderDetailsContainer}>
-            {newOrder.OrderDetails.map((detail, index) => (
-              <View
-                key={`${detail.AutopartId}-${index}`}
-                style={styles.orderDetail}
-              >
-                <Picker
-                  selectedValue={detail.AutopartId}
-                  style={styles.picker}
-                  onValueChange={(itemValue) => {
-                    const updatedOrderDetails = [...newOrder.OrderDetails];
-                    updatedOrderDetails[index].AutopartId = itemValue;
-                    setNewOrder({
-                      ...newOrder,
-                      OrderDetails: updatedOrderDetails,
-                    });
-                  }}
+        {/* Modal para crear un nuevo pedido */}
+        <Modal
+          visible={isModalVisible}
+          animationType="slide"
+          transparent={true}
+          onRequestClose={() => setIsModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalTitle}>Crear Nuevo Pedido</Text>
+              <TextInput
+                style={styles.input}
+                value={newOrder.OrderDate}
+                onChangeText={(text) =>
+                  setNewOrder({ ...newOrder, OrderDate: text })
+                }
+                placeholder="Fecha del Pedido"
+              />
+              <TextInput
+                style={styles.input}
+                value={newOrder.PaymentStatus}
+                onChangeText={(text) =>
+                  setNewOrder({ ...newOrder, PaymentStatus: text })
+                }
+                placeholder="Estado de Pago"
+              />
+              <TextInput
+                style={styles.input}
+                value={newOrder.ShippingAddress}
+                onChangeText={(text) =>
+                  setNewOrder({ ...newOrder, ShippingAddress: text })
+                }
+                placeholder="Dirección de Envío"
+              />
+              <TextInput
+                style={styles.input}
+                value={newOrder.ShippingStatus}
+                onChangeText={(text) =>
+                  setNewOrder({ ...newOrder, ShippingStatus: text })
+                }
+                placeholder="Estado de Envío"
+              />
+              <TextInput
+                style={styles.input}
+                value={newOrder.Observation}
+                onChangeText={(text) =>
+                  setNewOrder({ ...newOrder, Observation: text })
+                }
+                placeholder="Observaciones"
+              />
+              {newOrder.OrderDetails.map((detail, index) => (
+                <View
+                  key={index}
+                  style={{ flexDirection: "row", marginBottom: 10 }}
                 >
-                  {autoparts.map((autopart, index) => (
-                    <Picker.Item
-                      key={`${autopart.AutopartId}-${index}`}
-                      label={autopart.name}
-                      value={autopart.AutopartId}
-                    />
-                  ))}
-                </Picker>
-                <TextInput
-                  style={[styles.input, styles.quantityInput]}
-                  placeholder="Cantidad"
-                  onChangeText={(text) => {
-                    const updatedOrderDetails = [...newOrder.OrderDetails];
-                    updatedOrderDetails[index].Quantity = text;
-                    setNewOrder({
-                      ...newOrder,
-                      OrderDetails: updatedOrderDetails,
-                    });
-                  }}
-                />
-              </View>
-            ))}
-            <Button
-              title="Agregar Item"
-              onPress={() =>
-                setNewOrder({
-                  ...newOrder,
-                  OrderDetails: [
-                    ...newOrder.OrderDetails,
-                    {
-                      AutopartId: autoparts[0] ? autoparts[0].AutopartId : "",
-                      Quantity: "",
-                    },
-                  ],
-                })
-              }
-            />
+                  <Picker
+                    style={[styles.input, { flex: 1, marginRight: 10 }]}
+                    selectedValue={detail.AutopartId}
+                    onValueChange={(itemValue) =>
+                      handleDetailChange(index, "AutopartId", itemValue)
+                    }
+                  >
+                    <Picker.Item label="Selecciona una autoparte" value="" />
+                    {autoparts.map((autopart, autopartIndex) => (
+                      <Picker.Item
+                        key={autopartIndex} // Usando el índice como key único
+                        label={autopart.name}
+                        value={autopart.id}
+                      />
+                    ))}
+                  </Picker>
+                  <TextInput
+                    style={[styles.input, { flex: 1 }]}
+                    value={detail.Quantity.toString()}
+                    onChangeText={(text) =>
+                      handleDetailChange(index, "Quantity", parseInt(text))
+                    }
+                    placeholder="Cantidad"
+                    keyboardType="numeric"
+                  />
+                  <Button
+                    title="Eliminar"
+                    onPress={() => removeOrderDetail(index)}
+                  />
+                </View>
+              ))}
+              <Button title="Agregar Autoparte" onPress={addOrderDetail} />
+              {/* Agregar más campos de TextInput según sea necesario */}
+              <Button title="Guardar Pedido" onPress={handleCreateOrder} />
+              <Button
+                title="Cancelar"
+                onPress={() => setIsModalVisible(false)}
+              />
+            </View>
           </View>
-          <Button title="Crear" onPress={handleCreateOrder} />
-          <Button title="Cancelar" onPress={handleCloseModal} />
-        </View>
-      </Modal>
-    </View>
+        </Modal>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
   modalContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
   },
   modalTitle: {
     fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 20,
+    marginBottom: 10,
   },
   input: {
-    width: "80%",
     height: 40,
+    borderColor: "gray",
     borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-  orderDetailsContainer: {
-    width: "80%",
-    marginBottom: 20,
-  },
-  orderDetail: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  picker: {
-    flex: 1,
-    height: 40,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    marginRight: 10,
-  },
-  quantityInput: {
-    flex: 1,
-  },
-  orderItem: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    padding: 10,
     marginBottom: 10,
   },
 });
 
-export default Orders;
+export default Pedidos;
